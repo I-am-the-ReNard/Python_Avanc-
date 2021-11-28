9c9a8107619ece822b289ebf6acbb89fa73c77d0
I-am-the-ReNard <91788005+I-am-the-ReNard@users.noreply.github.com>
2021-11-28 11:10:17 +0100

    Correcting French mistakes.

--- Challenge3/main.py
+++ Challenge3/main.py
@@ -1,6 +1,6 @@
 from scapy.all import ARP,srp,Ether,get_if_addr,get_working_if
 
-###MÉTHODE : Idéntification de la MAC de l'IP envoyé###
+###MÉTHODE : Identification de l'adresse MAC de l'IP envoyée###
 def mac(ip):
     ans,_ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip),timeout=1, verbose=0)
     if ans:
@@ -9,19 +9,19 @@ def mac(ip):
         return False
 
 ###PROGRAMME###
-ip = get_if_addr(scapy.conf.iface)    #On obtient notre IP
+ip = get_if_addr(scapy.conf.iface)    #On obtient l'IP de notre interface
 print(ip)
 
-print(get_working_if().description)   #On cherche le type d'interface de réseau
+print(get_working_if().description)   #On obtient le type d'interface réseau
 
-ip = ip.split(".")  #Pour chercher les autres machines de notre réseau, on va efacer le dernier numéro de notre IP
+ip = ip.split(".")  #Pour chercher les autres machines de notre réseau on va effacer le dernier octet de notre adresse IP
 ip = f"{ip[0]}.{ip[1]}.{ip[2]}."
 
-print("DEMARRAGE DU SCAN")  #Avec une boucle, on trouvera toutes les adresses MAC
+print("DEMARRAGE DU SCAN")  #Avec une boucle, on trouvera toutes les adresses MAC des adresses IP trouvées
 k=1
 for i in range(1,254) :
-    out = mac(ip+str(i))    #Appelle à la méthode qui trouve les MAC par IP
+    out = mac(ip+str(i))    #On appelle la méthode qui trouve l'adresse MAC pour chaque IP
     if out != False :
-        print("MAC Adress "+str(k)+" : "+out)   #On affiche en console le résultat
+        print("MAC Adresse "+str(k)+" : "+out)   #On affiche en console le résultat
         k+=1
 print("FIN DU SCAN")
