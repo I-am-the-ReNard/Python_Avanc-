b9a59298ae47b37707195c904bf6baa56bdf4700
I-am-the-ReNard <91788005+I-am-the-ReNard@users.noreply.github.com>
2021-11-28 11:04:51 +0100

    Correcting French mistakes.

--- Challenge5/main.py
+++ Challenge5/main.py
@@ -3,7 +3,7 @@ import hashlib
 ###MÉTHODE : Cherche le mdp###
 def breakPass (motPass) :
     with open(r"mots.txt","r") as fp :    #On ouvre le document avec les possibles mdp
-        encr = [hashlib.md5,hashlib.sha256,hashlib.sha512] #Liste avec les méthodes d'encryptation qu'on utilisera
+        encr = [hashlib.md5,hashlib.sha256,hashlib.sha512] #Liste avec les méthodes d'encryption qu'on utilisera
         for en in encr :    #Boucle qui parcourt la liste des méthodes
             for line in fp :    #Boucle qui parcourt le document
                 if motPass == en(line[:-1].encode("UTF-8")).hexdigest() :  #Le mdp == le mot de la ligne "i" encrypté ?
@@ -11,10 +11,10 @@ def breakPass (motPass) :
             fp.seek(0)
         return ""
 ###PROGRAME###
-print("Veuilliez écriver un mot de pas faible : ")  #Définition du mdp
+print("Veuillez écrire un mot de passe faible : ")  #Définition du mdp
 motPass = input()
 
-print("Selectionnez une méthode d'encryptation : \n\t1) md5\n\t2) sha256\n\t3) sha512") #Définition de la méthode d'encryptation
+print("Selectionnez une méthode d'encryption : \n\t1) md5\n\t2) sha256\n\t3) sha512") #Définition de la méthode d'encryption
 sel = int(input())
 if sel == 3 :
     motPass = hashlib.sha512(motPass.encode("UTF-8")).hexdigest()   #md5
@@ -25,6 +25,6 @@ else :    #Par défaut
 
 mp = breakPass(motPass) #On appelle la fonction qui essayera d'obtenir le mdp
 if not mp :   #Résultats
-    print("Le mot de pas n'est pas trouvé")
+    print("Le mot de passe n'a pas été trouvé.")
 else :
-    print("Le mot de pass est : "+mp)
+    print("Le mot de passe est : "+mp)
